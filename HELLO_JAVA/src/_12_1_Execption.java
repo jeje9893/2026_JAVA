c7580d5df86ef13e3740bf56c4596956fc413375
SW410 <jeje9893@gmail.com>
2026-03-25 12:32:50 +0900

    직접 친 코드

--- HELLO_JAVA/src/_12_1_Execption.java
+++ HELLO_JAVA/src/_12_1_Execption.java
@@ -1,5 +1,46 @@
+import javax.annotation.processing.SupportedSourceVersion;
+
 public class _12_1_Execption {
     public static void main(String[] args) {
-        
+
+        /*
+        try
+        catch
+        finally
+         */
+
+
+        // int[] numbers = { 1, 2, 3};
+        // int index = 5;
+        // try {
+        //     int result = numbers[index];
+        // }
+        // catch (Exception e) {
+        //     System.out.println("_12_1Execption {0} Error");
+        // }
+
+        try {
+            int age  = -5;
+            if(age < 0)
+                throw new Exception("나이가 0살보다 작을 수 없습니다.");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            divide(3,0);
+        }
+        catch (Exception e) {
+            System.out.println("0으로 나눌 수 없습니다.");
+        }
+
+
+
+
+    }
+
+    public static int divide(int a, int b) throws Exception {
+        return a / b;
     }
 }
